b4c3ef989d2807e78fd2ffe2528e7cc09d0d1212
Shaik Asif <bhanurshaikasif@gmail.com>
2025-01-17 12:36:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function change() {
+       let p = document.getElementById("para");
+    p.textContent = "Entered Metaverse";
+    p.style.fontWeight="bold";
+    p.style.fontSize="20px";
+	
+}
